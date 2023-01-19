ac84a168ff701b9cff661aafe9eb9534cd6e6059
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-01-19 12:33:17 -0500

    built out discover option/select function, minimal functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,37 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genreTitle = "Pop";
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-between w-full mt-4 mb-10 sm:flex-row">
+        <h2 className="text-3xl font-bold text-left text-white">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="p-3 mt-5 text-sm text-gray-300 bg-black rounded-lg outline-none sm:mt-0"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>{genre.title}</option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {[1,2,3,4,5,6,7,8,9,10].map((song, i) => (
+          <SongCard
+          key={song.key}
+          song={song}
+          i={i}
+          />
+        ))}
+        
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
