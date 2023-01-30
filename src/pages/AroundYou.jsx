ab9c3ca74c376e5a3c8bb06cb2c40e6d706263da
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-01-30 15:27:18 -0500

    added in axios request for geo ipify api

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from "../components";
 
-export default CountryTracks;
+const AroundYou = () => {
+  const [country, setCountry] = useState("");
+  const [loading, setloading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  useEffect(() => {
+    axios.get(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_pwx7TfmU9cYUnH6sulnaqBiuDFiWL`
+    );
+  }, [country]);
+
+  return <div></div>;
+};
+
+export default AroundYou;
