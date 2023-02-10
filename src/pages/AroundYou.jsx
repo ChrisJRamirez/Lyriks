e2406a61b8d91969fabee234f39d8ef920c10d92
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-02-10 14:23:20 -0500

    fiished building around you, added in getsongbycountry query and Songcard component

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -4,11 +4,13 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
 const AroundYou = () => {
   const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const [data, isFetching, error] = useGetSongsByCountryQuery(country);
 
   console.log(country);
 
@@ -22,7 +24,29 @@ const AroundYou = () => {
       .finally(() => setLoading(false));
   }, [country]);
 
-  return <div></div>;
+  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="mt-4 mb-10 text-3xl font-bold text-left text-white">
+        Around You <span className="font-black">{country}</span>
+      </h2>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AroundYou;
