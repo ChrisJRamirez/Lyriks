875be9008d3897260ebd427a2e893ece8e67c72c
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-02-11 12:35:25 -0500

    fixed query error in around you page

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,38 +1,40 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
 import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
-const AroundYou = () => {
+const CountryTracks = () => {
   const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const [data, isFetching, error] = useGetSongsByCountryQuery(country);
-
-  console.log(country);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
     axios
       .get(
-        `https://geo.ipify.org/api/v2/country?apiKey=at_pwx7TfmU9cYUnH6sulnaqBiuDFiWL`
+        `https://geo.ipify.org/api/v2/country?apiKey=at_pwx7TfmU9cYUnH6sulnaqBiuDFiWL
+        `
       )
-      .then((res) => setCountry(res?.data?.location?.country))
+      .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, [country]);
 
-  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+  if (isFetching && loading)
+    return <Loader title="Loading Songs around you..." />;
 
-  if (error && country) return <Error />;
+  if (error && country !== "") return <Error />;
+
+  console.log(country);
 
   return (
     <div className="flex flex-col">
       <h2 className="mt-4 mb-10 text-3xl font-bold text-left text-white">
-        Around You <span className="font-black">{country}</span>
+        Around you <span className="font-black">{country}</span>
       </h2>
+
       <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
         {data?.map((song, i) => (
           <SongCard
@@ -49,4 +51,4 @@ const AroundYou = () => {
   );
 };
 
-export default AroundYou;
+export default CountryTracks;
